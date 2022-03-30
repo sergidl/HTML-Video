a1159beb1b54672d1fec24c879b8186617d53411
sergidl <sergidiazlopez3@gmail.com>
2022-03-30 18:24:29 +0200

    done

--- src/script.js
+++ src/script.js
@@ -1,14 +1,14 @@
 //option 2 with JQuery Smooth Scroll
 //https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
-$('.navbar a').click(function(e) {
-   if (this.hash !== ''){
-       e.preventDefault();
+$('.navbar a').click(function (e) {
+	if (this.hash !== '') {
+		e.preventDefault();
 
-       const hash=this.hash;
-       //('html, body') is the jquery solution for cross browser scroll animation
-       $("html, body").animate({
-           scrollTop: $(hash).offset().top
-       },800);
-   }
+		const hash = this.hash;
+		//('html, body') is the jquery solution for cross browser scroll animation
+		$("html, body").animate({
+			scrollTop: $(hash).offset().top
+		}, 800);
+	}
 })
 
